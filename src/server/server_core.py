d922d2914bb1a6f8bf0367935c50aa043dcd47ce
James <james.levi.rogers@google.com>
2024-11-25 11:11:13 -0600

    chore(api): Added envs utility to server setup

--- src/server/server_core.py
+++ src/server/server_core.py
@@ -7,17 +7,19 @@ from flask import Flask
 from flask_smorest import Api
 
 from src.server import blueprints, core
+from src.utils.envs import API_VERSION, ARCHIVE_PATH, HOST, PORT
 
 
-def main(host="0.0.0.0", port=5000, debug=True):
+def main(debug=True):
     """
     Server entry point.
     """
+
     app = Flask(__name__)
     app.logger.setLevel("DEBUG")
 
     app.config["API_TITLE"] = "Crafter API"
-    app.config["API_VERSION"] = "v1"
+    app.config["API_VERSION"] = f"v{API_VERSION}"
     app.config["OPENAPI_VERSION"] = "3.0.3"
     app.config["OPENAPI_JSON_PATH"] = "openapi.json"
     app.config["OPENAPI_URL_PREFIX"] = "/"
@@ -25,12 +27,9 @@ def main(host="0.0.0.0", port=5000, debug=True):
     app.config["OPENAPI_SWAGGER_UI_URL"] = (
         "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/3.25.0/"
     )
-    # app.config["LOG_FILE_PATH"] = LOG_FILE_PATH
 
+    atexit.register(core.init(ARCHIVE_PATH))
     api = Api(app)
-    path = f"{"/".join(__file__.split('/')[0:-3])}/data"
     blueprints.init(api)
-    atexit.register(core.init(path))
 
-    # Start the server
-    app.run(host=host, port=port, debug=debug)
+    app.run(host=HOST, port=PORT, debug=debug)
